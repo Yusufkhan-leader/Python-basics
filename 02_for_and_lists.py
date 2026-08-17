44f3845f964bc198bdb5bdcc4b62fad200e69179
Muhammadyusuf <mjalilov862@gmail.com>
2026-08-17 16:10:12 +0500

    Add grading logic for ball scores

---
+++ 02_for_and_lists.py
@@ -0,0 +1,7 @@
+ballar=[55, 80, 42, 90, 65]
+for ball in ballar:
+    print(f"To'plagan ball: {ball}")
+    if ball>=60:
+        print("Siz imtihondan o'tdingiz!")
+    else:
+        print("Siz imtihondan o'tolmadingiz.")    
