5faaaf57af2c002944e460653a39d9d7ae759fa3
Muhammadyusuf <mjalilov862@gmail.com>
2026-08-18 21:12:01 +0500

    Create 05_json_practice.py to save movie data as JSON

---
+++ 05_json_practice.py
@@ -0,0 +1,10 @@
+import json
+kinolar = [
+    {"nomi": "Inception", "baho": 8.8},
+    {"nomi": "Interstellar", "baho": 8.7},
+    {"nomi": "The Matrix", "baho": 8.7}
+]
+with open("kinolar.json", "w") as fayl:
+    json.dump(kinolar, fayl, indent=4)
+
+print("Ma'lumotlar JSON faylga saqlandi!")
