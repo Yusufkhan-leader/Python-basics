1c057b103718a898c4fc0bad66ef468adf7ef4b3
Muhammadyusuf <mjalilov862@gmail.com>
2026-08-18 13:45:43 +0500

    Add dictionary practice code

---
+++ 03_dictionaries.py
@@ -0,0 +1,9 @@
+kitob={
+    "nomi": "Idiot",
+    "muallif": "Fyodor Dostoyevsky",
+    "yili": 1883,
+    "janr": "psixologik roman",
+    "baho": 9.5
+}
+for k, v in kitob.items():
+    print(f"{k.title()}: {v}")
