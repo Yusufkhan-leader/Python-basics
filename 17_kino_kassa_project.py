9d1a09a318c0c2cc12900d7fa32931da9b379203
Muhammadyusuf <mjalilov862@gmail.com>
2026-08-23 14:53:43 +0500

    Add Kino and Kassa functionality to project

---
+++ 17_kino_kassa_project.py
@@ -0,0 +1,10 @@
+from main import Kino, Kassa
+kino1=Kino("Avatar", 40000)
+kino2=Kino("Inceptoin", 50000)
+kassa=Kassa(100000)
+print(kassa.balansni_korish())
+print(kassa.chipta_sot(kino1))
+print(kassa.chipta_sot(kino2))
+print(kassa.balansni_korish())
+
+    
